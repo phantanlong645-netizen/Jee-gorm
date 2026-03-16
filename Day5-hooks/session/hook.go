df2eccde59d67aa7fe7995c772930960dde94522
wixie <phantanlong645@gmail.com>
2026-03-16 16:37:58 +0800

    写完钩子函数

---
+++ Day5-hooks/session/hook.go
@@ -0,0 +1,35 @@
+package session
+
+import (
+	"Day5-hooks/log"
+	"reflect"
+)
+
+const (
+	BeforeQuery  = "BeforeQuery"
+	AfterQuery   = "AfterQuery"
+	BeforeUpdate = "BeforeUpdate"
+	AfterUpdate  = "AfterUpdate"
+	BeforeDelete = "BeforeDelete"
+	AfterDelete  = "AfterDelete"
+	BeforeInsert = "BeforeInsert"
+	AfterInsert  = "AfterInsert"
+)
+
+func (s *Session) CallMethod(method string, value interface{}) error {
+	fm := reflect.ValueOf(s.Reftable().Model).MethodByName(method)
+	if value != nil {
+		fm = reflect.ValueOf(value).MethodByName(method)
+	}
+	param := []reflect.Value{reflect.ValueOf(s)}
+	if fm.IsValid() {
+		v := fm.Call(param)
+		if len(v) > 0 {
+			if err, ok := v[0].Interface().(error); ok {
+				log.Error(err.Error())
+				return err
+			}
+		}
+	}
+	return nil
+}
---
+++ Day5-hooks/session/raw.go
@@ -0,0 +1,60 @@
+package session
+
+import (
+	"Day5-hooks/clause"
+	"database/sql"
+	"strings"
+
+	"Day5-hooks/dialect"
+	"Day5-hooks/schema"
+)
+
+type Session struct {
+	db       *sql.DB
+	dialect  dialect.Dialect
+	refTable *schema.Schema
+	sql      strings.Builder
+	sqlVars  []interface{}
+	clause   clause.Clause
+}
+
+func New(db *sql.DB, dialect dialect.Dialect) *Session {
+	return &Session{
+		db:      db,
+		dialect: dialect,
+	}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+	s.clause = clause.Clause{}
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	result, err = s.DB().Exec(s.sql.String(), s.sqlVars...)
+	return
+}
+
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	var rows *sql.Row
+	rows = s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+	return rows
+}
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	rows, err = s.DB().Query(s.sql.String(), s.sqlVars...)
+	return
+}
+
+func (s *Session) Raw(sql string, args ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, args...)
+	return s
+}
